9ee92d72e76b9bcbff728bb5c7b248311a38d9a3
Rafitos <rafaelsevla@gmail.com>
2025-12-09 17:14:26 -0300

    add errors matchers

--- rspec_tdd/spec/calculator/calculator_spec.rb
+++ rspec_tdd/spec/calculator/calculator_spec.rb
@@ -3,6 +3,15 @@ require 'calculator'
 describe Calculator, "Classe da Calculadora" do
   subject(:calc) { described_class.new() }
 
+  context '#div' do
+    it 'divide by 0' do
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3,0)}.to raise_error('divided by 0')
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError, 'divided by 0')
+      expect{subject.div(3,0)}.to raise_error(/divided/)
+    end
+  end
+
   context "#sum" do
     it "with positive numbers" do
       result = calc.sum(5, 7)
