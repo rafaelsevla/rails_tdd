a116330662d56cbce2a6021ebb21003ec6a475ec
Rafitos <rafaelsevla@gmail.com>
2025-12-10 10:07:55 -0300

    add hooks (before and after)

--- rspec_tdd/spec/matchers/attributes/attributes_spec.rb
+++ rspec_tdd/spec/matchers/attributes/attributes_spec.rb
@@ -1,27 +1,33 @@
 require 'people'
 
 describe 'Attributes' do
+  before (:each) do
+    @people = People.new
+  end
+
+  after(:each) do
+    @people.name = "Sem nome!"
+    puts ">>> #{@people.inspect}"
+  end
+
   context "have_attributes" do
     it 'simple' do
-      people = People.new
-      people.name = 'Rafa'
-      people.age = 27
+      @people.name = 'Rafa'
+      @people.age = 27
   
-      expect(people).to have_attributes(name: "Rafa", age: 27)
+      expect(@people).to have_attributes(name: "Rafa", age: 27)
     end
     it 'starting_with and be' do
-      people = People.new
-      people.name = 'Rafa'
-      people.age = 27
+      @people.name = 'Rafa'
+      @people.age = 27
   
-      expect(people).to have_attributes(name: starting_with('R'), age: (be >= 27))
+      expect(@people).to have_attributes(name: starting_with('R'), age: (be >= 27))
     end
     it 'a_string_starting_with and a_value' do
-      people = People.new
-      people.name = 'Rafa'
-      people.age = 27
+      @people.name = 'Rafa'
+      @people.age = 27
 
-      expect(people).to have_attributes(name: a_string_starting_with('R'), age: (a_value >= 27))
+      expect(@people).to have_attributes(name: a_string_starting_with('R'), age: (a_value >= 27))
     end
   end
 end
