a4223c725a9e490fbad7fb9e32114c2d8b87093d
Rafitos <rafaelsevla@gmail.com>
2025-12-10 09:20:57 -0300

    add one-liner syntax(subject)

--- rspec_tdd/spec/matchers/ranges/ranges_spec.rb
+++ rspec_tdd/spec/matchers/ranges/ranges_spec.rb
@@ -3,5 +3,12 @@ describe (1..5), 'Ranges' do
     expect(subject).to cover(2)
     expect(subject).to cover(2,5)
     expect(subject).not_to cover(0,6)
+    # is_expeted = expect(subject)
+    is_expected.to cover(2)
   end
+
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(2,5) }
+  it { is_expected.not_to cover(0,6) }
+  it { is_expected.to cover(2) }
 end
