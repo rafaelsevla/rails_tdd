1be8b7428e9a98e388f7c37c3284d05e8d849136
Rafitos <rafaelsevla@gmail.com>
2025-12-11 18:32:04 -0300

    add aggregate failures

--- rspec_tdd/spec/matchers/comparison/comparison_spec.rb
+++ rspec_tdd/spec/matchers/comparison/comparison_spec.rb
@@ -23,6 +23,12 @@ describe 'Matchers de Comparação' do
     expect(7).to be_between(2,7).inclusive
   end
 
+  # it 'be_between inclusive / Aggregate failures', :aggregate_failures do
+  #   expect(5).to be_between(2,7).inclusive
+  #   expect(1).to be_between(2,7).inclusive # esse teste falha
+  #   expect(8).to be_between(2,7).inclusive # esse teste falha
+  # end
+
   it 'be_between exclusive' do
     expect(5).to be_between(2,7).exclusive
     expect(3).to be_between(2,7).exclusive
