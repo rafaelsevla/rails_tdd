405ab3ff77f66c0e14e237dde81e655d98b24514
Rafitos <rafaelsevla@gmail.com>
2025-12-21 17:51:41 -0300

    add mock expectation

--- rspec_tdd/spec/mocks/mocks_spec.rb
+++ rspec_tdd/spec/mocks/mocks_spec.rb
@@ -9,4 +9,29 @@ describe 'Mocks' do
     # exercise
     student.bar
   end
+
+  it "args" do
+    student = Student.new
+    expect(student).to receive(:foo).with(42)
+
+    student.foo(42)
+  end
+
+  it "repetition" do
+    student = Student.new
+    expect(student).to receive(:foo).with(42).twice
+
+    student.foo(42)
+    student.foo(42)
+  end
+
+  it "with return" do
+    student = Student.new
+    expect(student).to receive(:foo).with(42).and_return("bar")
+    # return value is fake
+    # its a form of stubbing method
+
+    student.foo(42)
+    student.foo(42)
+  end
 end
