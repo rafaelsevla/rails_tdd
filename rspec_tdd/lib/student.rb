f9d8732e79d357d88751bc647a80708168aab618
Rafitos <rafaelsevla@gmail.com>
2025-12-21 17:34:22 -0300

    add mock with instance class and mock errors

--- rspec_tdd/lib/student.rb
+++ rspec_tdd/lib/student.rb
@@ -4,4 +4,12 @@ class Student
   def has_finished?(course)
     # true or false based on whether the student has finished the course
   end
+
+  def foo(arg)
+    #### some code
+  end
+  
+  def bar
+    #### some code
+  end
 end
--- rspec_tdd/spec/stubs/stubs_spec.rb
+++ rspec_tdd/spec/stubs/stubs_spec.rb
@@ -16,4 +16,37 @@ describe 'Stub' do
     finished_course = student.has_finished?(course)
     expect(finished_course).to be true
   end
+
+  it 'Dynamic Arguments' do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      if arg == :hello
+        "olá"
+      elsif arg == :hi
+        "oi"
+      end
+    end
+
+    expect(student.foo(:hello)).to eq("olá")
+    expect(student.foo(:hi)).to eq("oi")
+  end
+
+  it "Any instance of a class" do
+    student = Student.new
+    another_student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return('Palmeiras')
+
+    expect(student.bar).to eq('Palmeiras')
+    expect(another_student.bar).to eq('Palmeiras')
+  end
+
+  it "Mock Errors" do
+    student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_raise(RuntimeError)
+
+    expect { student.bar }.to raise_error(RuntimeError)
+  end
 end
