10120c07d0bf2274f6f7ce7bcd6c1ecb5ec6f3e1
Rafitos <rafaelsevla@gmail.com>
2025-12-21 17:54:16 -0300

    as null object

--- rspec_tdd/spec/double_tests/double_tests_spec.rb
+++ rspec_tdd/spec/double_tests/double_tests_spec.rb
@@ -1,15 +1,22 @@
 describe 'Double Test' do
-  it '--' do
+  it 'name' do
     user = double('User')
     allow(user).to receive_messages(name: 'John')
-
     user.name
   end
 
-  it '--' do
+  it 'password' do
     user = double('User')
     allow(user).to receive(:password).and_return('secret')
-
     user.password
+  end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive_messages(name: 'John')
+    allow(user).to receive(:password).and_return('secret')
+    user.name
+    user.password
+    user.abc # does not raise error
   end
 end
