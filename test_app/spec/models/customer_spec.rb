0af8f5718d0a85bf618be0d6f54f69cdd069b199
Rafitos <rafaelsevla@gmail.com>
2025-12-22 00:08:26 -0300

    add override attribute with factory

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe Customer, type: :model do
 
   it '#full_name' do
     customer = create(:customer)
+    expect(customer.full_name).to start_with('Mr. ')
+  end
 
-    expect(customer.full_name).to start_with('Mr. ').or start_with('Ms. ')
+  it '#full_name - override attribute' do
+    customer = create(:customer, name: "Alice Kingsleigh")
+    expect(customer.full_name).to start_with('Mr. ')
   end
 
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
