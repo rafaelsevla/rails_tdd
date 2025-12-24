cf8453f9a202af1a12d6a721ac6ca4ece97bef4c
Rafitos <rafaelsevla@gmail.com>
2025-12-24 14:43:23 -0300

    using vsr with metadata

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,11 +1,9 @@
 require 'httparty'
 
 describe 'HTTParty' do
-  it 'content-type' do
-    VCR.use_cassette("jsonplaceholder/posts") do
-      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-      content_type = response.headers['content-type']
-      expect(content_type).to match(/application\/json/)
-    end
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts' } do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
   end
 end
