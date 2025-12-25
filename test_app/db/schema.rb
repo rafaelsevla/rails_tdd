49548778e023d77b31f4df2ed8cc84c9ba29d389
Rafitos <rafaelsevla@gmail.com>
2025-12-25 14:38:00 -0300

    install devise on project

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_25_135539) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_25_173337) do
   create_table "categories", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
@@ -26,6 +26,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_25_135539) do
     t.integer "days_to_pay"
     t.string "gender"
     t.string "address"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
   create_table "orders", force: :cascade do |t|
