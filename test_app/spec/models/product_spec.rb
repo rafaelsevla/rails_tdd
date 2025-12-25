33dc3e6bdcc9d7bd4e696f1b32128f72649ea36d
Rafitos <rafaelsevla@gmail.com>
2025-12-25 14:13:41 -0300

    add shoulda matcher and refactor product_spec

--- test_app/spec/models/product_spec.rb
+++ test_app/spec/models/product_spec.rb
@@ -1,31 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  it 'is valid with description, price and category' do
-    product = create(:product)
-    expect(product).to be_valid
+RSpec.describe Product, type: :model do  
+  context 'rspec validates' do
+    it 'is valid with description, price and category' do
+      product = create(:product)
+      expect(product).to be_valid
+    end
+
+    it 'is invalid without description' do
+      product = build(:product, description: nil)
+      product.valid?
+      expect(product.errors[:description]).to include("can't be blank") 
+    end
+    
+    it 'is invalid without price' do
+      product = build(:product, price: nil)
+      product.valid?
+      expect(product.errors[:price]).to include("can't be blank") 
+    end
+    
+    it 'is invalid without category' do
+      product = build(:product, category: nil)
+      product.valid?
+      expect(product.errors[:category]).to include("can't be blank") 
+    end
+  end
+
+  context 'shoulda matchers validates' do
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:category) }
   end
   
-  it 'is invalid without description' do
-    product = build(:product, description: nil)
-    product.valid?
-    expect(product.errors[:description]).to include("can't be blank") 
+  context 'should matchers associations' do
+    it { is_expected.to belong_to(:category) }
   end
 
-  it 'is invalid without price' do
-    product = build(:product, price: nil)
-    product.valid?
-    expect(product.errors[:price]).to include("can't be blank") 
-  end
-
-  it 'is invalid without category' do
-    product = build(:product, category: nil)
-    product.valid?
-    expect(product.errors[:category]).to include("can't be blank") 
-  end
-
-  it 'return a product with full description' do
-    product = create(:product, description: 'Labubu', price: "15.0")
-    expect(product.full_description).to eq("Labubu - 15.0")
+  context 'Instance methods' do
+    it '#full_description' do
+      product = create(:product, description: 'Labubu', price: "15.0")
+      expect(product.full_description).to eq("Labubu - 15.0")
+    end
   end
 end
--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -31,6 +31,14 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
 
+  # Shoulda Matchers
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
   # Time Helper
   config.include ActiveSupport::Testing::TimeHelpers
 
