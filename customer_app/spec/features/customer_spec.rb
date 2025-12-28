c97b67ea2f52453625bd8e5dbc11f03f3b69507b
Rafitos <rafaelsevla@gmail.com>
2025-12-28 18:38:20 -0300

    add register customer spec with i18n

--- customer_app/spec/features/customer_spec.rb
+++ customer_app/spec/features/customer_spec.rb
@@ -19,4 +19,19 @@ feature "Customers", type: :feature do
     click_on('Novo Cliente')
     expect(page).to have_content('Novo Clientee')
   end
+
+  scenario 'Register a new customer' do
+    visit(new_customer_path)
+    customer_name = Faker::Name.name
+
+    fill_in('customer_name', with: customer_name)
+    fill_in('customer_email', with: Faker::Internet.email)
+    fill_in('customer_phone', with: Faker::PhoneNumber.phone_number)
+    attach_file('customer_avatar', "#{Rails.root}/spec/fixtures/avatar.png")
+    choose(option: [ 'N', 'S' ].sample)
+    click_on('Criar Cliente')
+
+    expect(page).to have_content('Cliente cadastrado com sucesso!')
+    expect(Customer.last.name).to eq(customer_name)
+  end
 end
