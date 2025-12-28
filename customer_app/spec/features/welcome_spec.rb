2c2e38b35ba99b5b7540840930e3bb2bf29ce719
Rafitos <rafaelsevla@gmail.com>
2025-12-28 17:54:57 -0300

    add test for link

--- customer_app/spec/features/welcome_spec.rb
+++ customer_app/spec/features/welcome_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Welcome", type: :feature do
   scenario "Display welcome message" do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Bem-Vindo')
+  end
+
+  scenario "Display login link" do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
   end
 end
