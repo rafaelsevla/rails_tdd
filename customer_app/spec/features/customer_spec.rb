53f625250d40c7ab4611820c23f881b2688c4f29
Rafitos <rafaelsevla@gmail.com>
2025-12-28 19:28:42 -0300

    add click on show link

--- customer_app/spec/features/customer_spec.rb
+++ customer_app/spec/features/customer_spec.rb
@@ -94,4 +94,18 @@ feature "Customers", type: :feature do
     expect(page).to have_content('Cliente atualizado com sucesso')
     expect(page).to have_content(new_name)
   end
+
+  scenario 'Click on "show" link' do
+    Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: [ 'N', 'S' ].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr[1]/td[2]/a").click
+    expect(page).to have_content("Detalhamento do Cliente")
+  end
 end
